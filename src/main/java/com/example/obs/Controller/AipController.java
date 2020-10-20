5fa57b5534aa939590703d593f6e87579d574219
zhuyanwu <ywzhup@isoftstone.com>
2020-10-20 15:28:06 +0800

    baidu aip

---
+++ src/main/java/com/example/obs/Controller/AipController.java
@@ -0,0 +1,43 @@
+package com.example.obs.Controller;
+
+import com.baidu.aip.ocr.AipOcr;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+
+@Controller
+@RequestMapping("/aip")
+public class AipController {
+
+    private static final Logger logger = LoggerFactory.getLogger(AipController.class);
+
+    private static final String appId = "22846466";
+
+    private static final String ak = "Rlg50PDYqM3ePuAKRXGXEYI1";
+
+    private static final String sk = "S3xcN9iuiQMIaN012eA2zQS23DBzjzgc";
+
+    @GetMapping("/test")
+    @ResponseBody
+    public String test(){
+        AipOcr client = new AipOcr(appId,ak,sk);
+        // 传入可选参数调用接口
+        HashMap<String, String> options = new HashMap<String, String>();
+        options.put("language_type", "CHN_ENG");
+        options.put("detect_direction", "true");
+        options.put("detect_language", "true");
+        options.put("probability", "true");
+
+        // 参数为本地图片路径
+        String image = "1603166382.jpg";
+        JSONObject res = client.basicGeneral(image, options);
+        return res.toString(2);
+    }
+
+}
--- src/main/java/com/example/obs/Controller/ObsController.java
+++ src/main/java/com/example/obs/Controller/ObsController.java
@@ -11,26 +11,28 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
 
 @RestController
+@RequestMapping("/obs")
 public class ObsController {
 
     private static final Logger logger = LoggerFactory.getLogger(ObsController.class);
 
-    private static final String endPoint = "obs.cn-east-3.myhuaweicloud.com";
-
-    private static final String ak = "HXK66CXIVKJBZ5KWQXVP";
-
-    private static final String sk = "o5DfcTF2VBFvGxPmAYB1bABxlteOQU4a97iO67Rk";
-
-//    private static final String endPoint = "obs.cn-north-4.myhuaweicloud.com";
+//    private static final String endPoint = "obs.cn-east-3.myhuaweicloud.com";
 //
-//    private static final String ak = "01EIF6LVEFYG5APFIVI9";
+//    private static final String ak = "HXK66CXIVKJBZ5KWQXVP";
 //
-//    private static final String sk = "pTfcxU2DQlaIYAXmgyboAOeA6DZZRiKQB4QHxY39";
+//    private static final String sk = "o5DfcTF2VBFvGxPmAYB1bABxlteOQU4a97iO67Rk";
+
+    private static final String endPoint = "obs.cn-north-4.myhuaweicloud.com";
+
+    private static final String ak = "01EIF6LVEFYG5APFIVI9";
+
+    private static final String sk = "pTfcxU2DQlaIYAXmgyboAOeA6DZZRiKQB4QHxY39";
 
     @GetMapping("/test")
     public String test(){
@@ -40,8 +42,9 @@ public class ObsController {
 //        config.setEndPoint(endPoint);
 
         ObsClient obsClient = new ObsClient(ak,sk,endPoint);
-//        HeaderResponse response = obsClient.putObject("zhu-bucket", "book2", new File("D:\\gitProject\\book2.txt"));
-        ObjectListing objectListing =obsClient.listObjects("zhu-bucket");
+//        HeaderResponse response = obsClient.putObject("zhu-bucket", "zhuyanwu/book1", new File("D:\\gitProject\\book2.txt"));
+//        obsClient.deleteObject("tongtong-img","file/1_1603101255352.txt");
+        ObjectListing objectListing =obsClient.listObjects("tongtong-img");
         logger.info("size: " + objectListing.getObjects().size());
         for(ObsObject obj : objectListing.getObjects()){
             logger.info(obj.getObjectKey());
